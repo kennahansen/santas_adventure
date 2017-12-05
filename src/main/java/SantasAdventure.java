b1043e040e46ac89dbdfdfcad7280d6d74e84070
whysafish108 <31488018+whysafish108@users.noreply.github.com>
2017-12-05 16:25:56 -0600

    added goHouse..... hopefully nothing is broken

--- src/main/java/SantasAdventure.java
+++ src/main/java/SantasAdventure.java
@@ -127,6 +127,47 @@ public class SantasAdventure {
 	 * main method for exploring the house.
 	 */
 	public static void goHouse() {
+		 System.out.println("The children were nestled all snug in their beds,"); 
+		 System.out.println("While visions of sugar-plums danced in their heads.");
+		 System.out.println("But then ! unexpectedly, you hear the sound of a child waking up !!!!!");
+		 System.out.println("Do you: [1] Hide [2] Stuff stockings quickly! even though it's messy "
+		 		+ "[3] speak with the child");
+		int houseOneNum = sc.nextInt();
+	     switch (houseTwoNum) {
+	     		case 1: System.out.println("\"You are santa and hiding!! this is pathetic\"");
+	     			santa.decrementHolidaySpirit();
+	     		case 2: System.out.println("\"Wow you really shouldn't have done that so messily!"
+	     			+ "I though Santa was supposed to be detail oriented...\"");
+	     			santa.decrementHolidaySpirit();
+	     		case 3: System.out.println("You say to the child: \" You can hear on the roof" + 
+	     			"The prancing and pawing of each little hoof.");
+	     			System.out.println("I'll speak not a word, but go straight to my work,\n" + 
+	     					"And fill all these stockings, and turn with a jerk.\n" + 
+	     					"And laying my finger aside of my nose,\n" + 
+	     					"And giving a nod, up the chimney I gooooooooooooo\"");
+	     			santa.incrementHolidaySpirit();
+	     		default:
+	     	System.out.println("Mamma in her ‘kerchief, and Pappa in his cap,\n" + 
+	     			"Had just settled their brains for a long winter’s nap.");
+	     	System.out.println("but luckily, they left some cookies and carrots for you and your reindeers!!!");
+	     	System.out.println("Would you like to [1] take the cookies [2] leave the cookies");
+		int houseTwoNum = sc.nextInt();
+		    switch (houseTwoNum) {
+		    			case 1: System.out.println("yummmmmmmm good choice !!!");
+		    				santa.incrementHolidaySpirit();
+		    				santa.incrementWarmth();
+		    				santa.incrementCheer();
+		    				reindeer.incrementHealth();
+		    			case 2: System.out.println("whyyyyyyyyyyyy");
+		    				santa.decrementHolidaySpirit();
+		    				santa.decrementWarmth();
+		    				santa.decrementCheer();
+		    				reindeer.decrementHealth();
+		    			default:
+		    }
+		System.out.println("You're CHEER is now: " + santa.getCheer());
+		System.out.println("Your HOLIDAY SPIRIT is now: " + santa.getHolidaySpirit());
+	     }
 	}
 
 	public static void goApartment() {
